cb5c083d16ef759f8ffc38127db8d2b1eeab276b
Sapphire Mason-Brown <saphmb@gmail.com>
2017-07-25 16:26:43 +0100

    First user story

--- Gemfile
+++ Gemfile
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 source "https://rubygems.org"
 
-git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
-
+gem 'capybara'
 gem 'data_mapper'
 gem 'dm-migrations'
 gem 'dm-postgres-adapter'
+gem 'sinatra'
 gem 'rspec'
---
+++ spec/features/view_links_spec.rb
@@ -0,0 +1,10 @@
+feature 'can view vinks on homepage' do
+  scenario 'viewing links on homepage' do
+    Link.create(url:'http://www.google.com', title: 'Google')
+    visit '/links'
+    expect(page.status_code).to eq 200
+    within 'ul#links' do
+      expect(page).to have_content('Google')
+    end
+  end
+end
